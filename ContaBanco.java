9ccbce99f0b845a9f83e256acf8d0344986d6420
kaikinattandossantos <156141207+kaikinattandossantos@users.noreply.github.com>
2024-12-07 15:55:13 -0300

    Update ContaBanco.java
    Push the real code

--- ContaBanco.java
+++ ContaBanco.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class ContaBanco {
     public static void main(String[] args) {
-        System.out.println("hello word");
+
+        Scanner scanner = new Scanner(System.in); 
+
+        System.out.println("Welcome to Bank of Brazil! Please, insert your name ");
+    
+        String name = scanner.nextLine();
+        System.out.println("Great " + name + "! What is your bank account?");
+
+        int id = scanner.nextInt();
+        System.out.println("Very nice");
+        System.out.println("Mr. "+ name + " you can use your bank account " + id + " now");
+
     }
 }
