734419c4202018d4bdf3640d98c4fc13f6e1ab64
kaiki nattan dos santos guedes <156141207+kaikinattandossantos@users.noreply.github.com>
2024-12-07 20:53:44 -0300

    Implemented a choice logic with switch and clearscreen

--- ContaBanco.java
+++ ContaBanco.java
@@ -1,18 +1,73 @@
 import java.util.Scanner;
 
-public class ContaBanco {
+public class BankAccount {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in); 
+        // List of options
+        String[] options = {"transfer", "balance", "deposit"};
+        CheckingAccount commonAccount = new CheckingAccount();
+        SavingsAccount inheritanceAccount = new SavingsAccount();
 
-        System.out.println("Welcome to Bank of Brazil! Please, insert your name ");
-    
+        // Request user's name
+        System.out.println("Welcome to Bank of Brazil! Please, enter your name:");
         String name = scanner.nextLine();
-        System.out.println("Great " + name + "! What is your bank account?");
 
+        // Clear the screen
+        clearScreen();
+
+        // Request bank account number
+        System.out.println("Great " + name + "! What is your bank account number?");
         int id = scanner.nextInt();
-        System.out.println("Very nice");
-        System.out.println("Mr. "+ name + " you can use your bank account " + id + " now");
 
+        // Clear the screen again
+        clearScreen();
+
+        // Display a message with the provided information
+        System.out.println("Very nice");
+        System.out.println("Mr. " + name + ", you can now use your bank account " + id);
+
+        // Clear the screen
+        clearScreen();
+        System.out.println("Choose the type of account:");
+        System.out.println("[1] Checking account. [2] Savings account");
+        int accountType = scanner.nextInt();
+
+        switch (accountType) {
+            case 1:
+                System.out.println("You selected the checking account.");
+                break;
+            case 2:
+                System.out.println("You selected the savings account.");
+                break;
+            default:
+                System.out.println("Invalid option.");
+        }
+
+        // Present options
+        System.out.println("Please, choose an option:");
+        System.out.println("[1] to transfer. [2] for balance. [3] to deposit.");
+
+        int usageOption = scanner.nextInt();
+
+        switch (usageOption) {
+            case 1:
+                System.out.println("You have chosen to transfer.");
+                break;
+            case 2:
+                System.out.println("You have chosen to check your balance.");
+                break;
+            case 3:
+                System.out.println("You have chosen to deposit.");
+                break;
+            default:
+                System.out.println("Invalid option.");
+        }
+    }
+
+    // Method to clear the screen
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
